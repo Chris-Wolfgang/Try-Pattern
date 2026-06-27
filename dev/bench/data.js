624c68614f15a129a7e596d9e10477cd55e1d441
github-action-benchmark <github@users.noreply.github.com>
2026-06-27 21:14:34 +0000

    add BenchmarkDotNet (benchmarkdotnet) benchmark result for b3a840b09437035ab14b8c7795de48e2dac9e38a

---
+++ dev/bench/data.js
@@ -0,0 +1,128 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1782594874237,
+  "repoUrl": "https://github.com/Chris-Wolfgang/Try-Pattern",
+  "entries": {
+    "BenchmarkDotNet": [
+      {
+        "commit": {
+          "author": {
+            "email": "210299580+Chris-Wolfgang@users.noreply.github.com",
+            "name": "Chris Wolfgang",
+            "username": "Chris-Wolfgang"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b3a840b09437035ab14b8c7795de48e2dac9e38a",
+          "message": "Merge pull request #166 from Chris-Wolfgang/vNext\n\nRelease v0.3.2: canonical maintenance round + AssemblyVersion fix",
+          "timestamp": "2026-06-27T17:10:29-04:00",
+          "tree_id": "ebd5c5362e24c12f66f744973cc4364cf6975c48",
+          "url": "https://github.com/Chris-Wolfgang/Try-Pattern/commit/b3a840b09437035ab14b8c7795de48e2dac9e38a"
+        },
+        "date": 1782594872471,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Wolfgang.TryPattern.Benchmarks.TryBenchmarks.Action_Success",
+            "value": 2501.273443222046,
+            "unit": "ns",
+            "range": "± 5.122858801890151"
+          },
+          {
+            "name": "Wolfgang.TryPattern.Benchmarks.TryBenchmarks.Action_WithException",
+            "value": 9789196.69140625,
+            "unit": "ns",
+            "range": "± 10925.762752249604"
+          },
+          {
+            "name": "Wolfgang.TryPattern.Benchmarks.TryBenchmarks.RunAsync_Action_Success",
+            "value": 693283.339453125,
+            "unit": "ns",
+            "range": "± 7607.952808415729"
+          },
+          {
+            "name": "Wolfgang.TryPattern.Benchmarks.TryBenchmarks.RunAsync_Action_WithException",
+            "value": 48993311.00645162,
+            "unit": "ns",
+            "range": "± 1494080.8164064111"
+          },
+          {
+            "name": "Wolfgang.TryPattern.Benchmarks.TryBenchmarks.Run_Func_Success",
+            "value": 9710.254013061523,
+            "unit": "ns",
+            "range": "± 41.93233071363464"
+          },
+          {
+            "name": "Wolfgang.TryPattern.Benchmarks.TryBenchmarks.Run_Func_WithException",
+            "value": 9755213.1,
+            "unit": "ns",
+            "range": "± 29402.558866039995"
+          },
+          {
+            "name": "Wolfgang.TryPattern.Benchmarks.TryBenchmarks.RunAsync_Func_Success",
+            "value": 47370.66650390625,
+            "unit": "ns",
+            "range": "± 288.6867121580512"
+          },
+          {
+            "name": "Wolfgang.TryPattern.Benchmarks.TryBenchmarks.RunAsync_Func_WithException",
+            "value": 21793111.09598214,
+            "unit": "ns",
+            "range": "± 106154.11349330173"
+          },
+          {
+            "name": "Wolfgang.TryPattern.Benchmarks.TryBenchmarks.Action_Success",
+            "value": 2191.1768913269043,
+            "unit": "ns",
+            "range": "± 2.6780552622815246"
+          },
+          {
+            "name": "Wolfgang.TryPattern.Benchmarks.TryBenchmarks.Action_WithException",
+            "value": 9906356.197916666,
+            "unit": "ns",
+            "range": "± 73123.69819301521"
+          },
+          {
+            "name": "Wolfgang.TryPattern.Benchmarks.TryBenchmarks.RunAsync_Action_Success",
+            "value": 698820.7652994791,
+            "unit": "ns",
+            "range": "± 6860.5380115459875"
+          },
+          {
+            "name": "Wolfgang.TryPattern.Benchmarks.TryBenchmarks.RunAsync_Action_WithException",
+            "value": 50406814.03030303,
+            "unit": "ns",
+            "range": "± 1203222.114257168"
+          },
+          {
+            "name": "Wolfgang.TryPattern.Benchmarks.TryBenchmarks.Run_Func_Success",
+            "value": 9938.262247721354,
+            "unit": "ns",
+            "range": "± 90.3326959139449"
+          },
+          {
+            "name": "Wolfgang.TryPattern.Benchmarks.TryBenchmarks.Run_Func_WithException",
+            "value": 9866183.661458334,
+            "unit": "ns",
+            "range": "± 42577.10099260121"
+          },
+          {
+            "name": "Wolfgang.TryPattern.Benchmarks.TryBenchmarks.RunAsync_Func_Success",
+            "value": 46779.46221923828,
+            "unit": "ns",
+            "range": "± 549.827278179399"
+          },
+          {
+            "name": "Wolfgang.TryPattern.Benchmarks.TryBenchmarks.RunAsync_Func_WithException",
+            "value": 21830051.927083332,
+            "unit": "ns",
+            "range": "± 130646.3942251359"
+          }
+        ]
+      }
+    ]
+  }
+}
